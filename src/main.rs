38920138cee7f345a4e594e4389c1954fafbe880
Grigorii Matiukhin <grigory36m@gmail.com>
2023-07-23 19:22:14 +0300

    camera

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::{app::AppExit, prelude::*};
 use bevy_egui::EguiPlugin;
 use terrain_procgen::generation::*;
 
+mod camera;
 mod ui;
 
 fn main() {
@@ -10,6 +11,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
         .add_plugins(MarchingCubesTerrain)
+        .add_plugins(camera::CameraPlugin)
         .add_event::<AppExit>()
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Update, ui::ui_system)
